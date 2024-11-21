fc3b80bbe7765d643df83b2bd21d7e7a5ecdb3b6
Steven <stevenstokes1020@gmail.com>
2024-11-20 21:53:07 -0500

    Fixed format errors and set steam only on Deimos.

--- Modules/Configs/neovim.nix
+++ Modules/Configs/neovim.nix
@@ -179,6 +179,18 @@
             config =
               # lua
               toLua ''
+                require("telescope").setup
+                {
+                  defaults =
+                  {
+                    vimgrep_arguments =
+                    {
+                      'rg',
+                      '--no-ignore',
+                    };
+                  };
+                };
+
                 -- Rebind commands
                 vim.api.nvim_create_user_command('FF', 'Telescope find_files', {})
                 vim.cmd('cnoreabbrev ff FF')
