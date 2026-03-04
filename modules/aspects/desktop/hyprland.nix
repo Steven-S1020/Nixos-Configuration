103f457f6a9faf84b14a8439dbf87138863bce1a
Steven <stevenstokes1020@gmail.com>
2026-03-04 01:57:37 -0500

    Added cool hyprland script to toggle opacity

--- modules/aspects/desktop/hyprland.nix
+++ modules/aspects/desktop/hyprland.nix
@@ -55,6 +55,31 @@
                   --actions-on-escape exit \
                   --early-exit
               '';
+
+              toggleTransparent = pkgs.writeShellScriptBin "toggle-transparent" ''
+                get_option() {
+                  hyprctl getoption decoration:"$1" | awk 'NR==1{print $2}'
+                }
+
+                set_option() {
+                  hyprctl --batch "\
+                    keyword decoration:active_opacity $1;\
+                    keyword decoration:inactive_opacity $2"
+                  exit
+                }
+
+                toggle_blur() {
+                  hyprctl keyword decoration:blur:enabled "$1"
+                }
+
+                if [ "$(get_option active_opacity)" = "0.000000" ]; then
+                  toggle_blur yes
+                  hyprctl reload
+                else
+                  toggle_blur no
+                  set_option 0 0
+                fi
+              '';
             in
             {
               wayland.windowManager.hyprland = {
@@ -117,6 +142,7 @@
                     "$mod, Z, togglespecialworkspace, mincontainer"
                     "$mod, Z, movetoworkspace, special:mincontainer"
                     "$mod, Z, togglespecialworkspace, mincontainer"
+                    "$mod, H, exec, ${toggleTransparent}/bin/toggle-transparent"
                     "Alt_L, H, movefocus, l"
                     "Alt_L, J, movefocus, d"
                     "Alt_L, K, movefocus, u"
